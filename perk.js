aa9bcbb51dc93d9ae2f17070d88c4004a32b67c2
IamOhim <168752525+IamOhim@users.noreply.github.com>
2024-09-22 21:58:06 +0700

    Add files via upload

---
+++ perk.js
@@ -0,0 +1,153 @@
+// Simulated novel content (portion of 50,000 words)
+const novelText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget venenatis quam, 
+                    non dapibus magna. Curabitur fringilla felis in dui fringilla efficitur. Aliquam erat volutpat. 
+                    Nam eget lorem eu orci rutrum lacinia. Donec ultricies augue non urna suscipit, 
+                    quis ornare libero suscipit. Phasellus facilisis sit amet justo eget ullamcorper. 
+                    Sed eu augue ac lacus lacinia sodales. Fusce varius libero eget felis egestas, a elementum nisi posuere.
+                    Suspendisse potenti. Quisque sit amet nibh ac arcu volutpat scelerisque. 
+                    Etiam id ultricies ex, non laoreet leo. Pellentesque a fringilla metus, sed maximus augue. 
+                    Vivamus congue vehicula dui, a fermentum libero condimentum ac. 
+                    Mauris sit amet ante urna. Etiam laoreet nibh a purus condimentum, at pharetra ligula fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget venenatis quam, 
+                    non dapibus magna. Curabitur fringilla felis in dui fringilla efficitur. Aliquam erat volutpat. 
+                    Nam eget lorem eu orci rutrum lacinia. Donec ultricies augue non urna suscipit, 
+                    quis ornare libero suscipit. Phasellus facilisis sit amet justo eget ullamcorper. 
+                    Sed eu augue ac lacus lacinia sodales. Fusce varius libero eget felis egestas, a elementum nisi posuere.
+                    Suspendisse potenti. Quisque sit amet nibh ac arcu volutpat scelerisque. 
+                    Etiam id ultricies ex, non laoreet leo. Pellentesque a fringilla metus, sed maximus augue. 
+                    Vivamus congue vehicula dui, a fermentum libero condimentum ac. 
+                    Mauris sit amet ante urna. Etiam laoreet nibh a purus condimentum, at pharetra ligula fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget venenatis quam, 
+                    non dapibus magna. Curabitur fringilla felis in dui fringilla efficitur. Aliquam erat volutpat. 
+                    Nam eget lorem eu orci rutrum lacinia. Donec ultricies augue non urna suscipit, 
+                    quis ornare libero suscipit. Phasellus facilisis sit amet justo eget ullamcorper. 
+                    Sed eu augue ac lacus lacinia sodales. Fusce varius libero eget felis egestas, a elementum nisi posuere.
+                    Suspendisse potenti. Quisque sit amet nibh ac arcu volutpat scelerisque. 
+                    Etiam id ultricies ex, non laoreet leo. Pellentesque a fringilla metus, sed maximus augue. 
+                    Vivamus congue vehicula dui, a fermentum libero condimentum ac. 
+                    Mauris sit amet ante urna. Etiam laoreet nibh a purus condimentum, at pharetra ligula fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget venenatis quam, 
+                    non dapibus magna. Curabitur fringilla felis in dui fringilla efficitur. Aliquam erat volutpat. 
+                    Nam eget lorem eu orci rutrum lacinia. Donec ultricies augue non urna suscipit, 
+                    quis ornare libero suscipit. Phasellus facilisis sit amet justo eget ullamcorper. 
+                    Sed eu augue ac lacus lacinia sodales. Fusce varius libero eget felis egestas, a elementum nisi posuere.
+                    Suspendisse potenti. Quisque sit amet nibh ac arcu volutpat scelerisque. 
+                    Etiam id ultricies ex, non laoreet leo. Pellentesque a fringilla metus, sed maximus augue. 
+                    Vivamus congue vehicula dui, a fermentum libero condimentum ac. 
+                    Mauris sit amet ante urna. Etiam laoreet nibh a purus condimentum, at pharetra ligula fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget venenatis quam, 
+                    non dapibus magna. Curabitur fringilla felis in dui fringilla efficitur. Aliquam erat volutpat. 
+                    Nam eget lorem eu orci rutrum lacinia. Donec ultricies augue non urna suscipit, 
+                    quis ornare libero suscipit. Phasellus facilisis sit amet justo eget ullamcorper. 
+                    Sed eu augue ac lacus lacinia sodales. Fusce varius libero eget felis egestas, a elementum nisi posuere.
+                    Suspendisse potenti. Quisque sit amet nibh ac arcu volutpat scelerisque. 
+                    Etiam id ultricies ex, non laoreet leo. Pellentesque a fringilla metus, sed maximus augue. 
+                    Vivamus congue vehicula dui, a fermentum libero condimentum ac. 
+                    Mauris sit amet ante urna. Etiam laoreet nibh a purus condimentum, at pharetra ligula fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget venenatis quam, 
+                    non dapibus magna. Curabitur fringilla felis in dui fringilla efficitur. Aliquam erat volutpat. 
+                    Nam eget lorem eu orci rutrum lacinia. Donec ultricies augue non urna suscipit, 
+                    quis ornare libero suscipit. Phasellus facilisis sit amet justo eget ullamcorper. 
+                    Sed eu augue ac lacus lacinia sodales. Fusce varius libero eget felis egestas, a elementum nisi posuere.
+                    Suspendisse potenti. Quisque sit amet nibh ac arcu volutpat scelerisque. 
+                    Etiam id ultricies ex, non laoreet leo. Pellentesque a fringilla metus, sed maximus augue. 
+                    Vivamus congue vehicula dui, a fermentum libero condimentum ac. 
+                    Mauris sit amet ante urna. Etiam laoreet nibh a purus condimentum, at pharetra ligula fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget venenatis quam, 
+                    non dapibus magna. Curabitur fringilla felis in dui fringilla efficitur. Aliquam erat volutpat. 
+                    Nam eget lorem eu orci rutrum lacinia. Donec ultricies augue non urna suscipit, 
+                    quis ornare libero suscipit. Phasellus facilisis sit amet justo eget ullamcorper. 
+                    Sed eu augue ac lacus lacinia sodales. Fusce varius libero eget felis egestas, a elementum nisi posuere.
+                    Suspendisse potenti. Quisque sit amet nibh ac arcu volutpat scelerisque. 
+                    Etiam id ultricies ex, non laoreet leo. Pellentesque a fringilla metus, sed maximus augue. 
+                    Vivamus congue vehicula dui, a fermentum libero condimentum ac. 
+                    Mauris sit amet ante urna. Etiam laoreet nibh a purus condimentum, at pharetra ligula fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget venenatis quam, 
+                    non dapibus magna. Curabitur fringilla felis in dui fringilla efficitur. Aliquam erat volutpat. 
+                    Nam eget lorem eu orci rutrum lacinia. Donec ultricies augue non urna suscipit, 
+                    quis ornare libero suscipit. Phasellus facilisis sit amet justo eget ullamcorper. 
+                    Sed eu augue ac lacus lacinia sodales. Fusce varius libero eget felis egestas, a elementum nisi posuere.
+                    Suspendisse potenti. Quisque sit amet nibh ac arcu volutpat scelerisque. 
+                    Etiam id ultricies ex, non laoreet leo. Pellentesque a fringilla metus, sed maximus augue. 
+                    Vivamus congue vehicula dui, a fermentum libero condimentum ac. 
+                    Mauris sit amet ante urna. Etiam laoreet nibh a purus condimentum, at pharetra ligula fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget venenatis quam, 
+                    non dapibus magna. Curabitur fringilla felis in dui fringilla efficitur. Aliquam erat volutpat. 
+                    Nam eget lorem eu orci rutrum lacinia. Donec ultricies augue non urna suscipit, 
+                    quis ornare libero suscipit. Phasellus facilisis sit amet justo eget ullamcorper. 
+                    Sed eu augue ac lacus lacinia sodales. Fusce varius libero eget felis egestas, a elementum nisi posuere.
+                    Suspendisse potenti. Quisque sit amet nibh ac arcu volutpat scelerisque. 
+                    Etiam id ultricies ex, non laoreet leo. Pellentesque a fringilla metus, sed maximus augue. 
+                    Vivamus congue vehicula dui, a fermentum libero condimentum ac. 
+                    Mauris sit amet ante urna. Etiam laoreet nibh a purus condimentum, at pharetra ligula fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget venenatis quam, 
+                    non dapibus magna. Curabitur fringilla felis in dui fringilla efficitur. Aliquam erat volutpat. 
+                    Nam eget lorem eu orci rutrum lacinia. Donec ultricies augue non urna suscipit, 
+                    quis ornare libero suscipit. Phasellus facilisis sit amet justo eget ullamcorper. 
+                    Sed eu augue ac lacus lacinia sodales. Fusce varius libero eget felis egestas, a elementum nisi posuere.
+                    Suspendisse potenti. Quisque sit amet nibh ac arcu volutpat scelerisque. 
+                    Etiam id ultricies ex, non laoreet leo. Pellentesque a fringilla metus, sed maximus augue. 
+                    Vivamus congue vehicula dui, a fermentum libero condimentum ac. 
+                    Mauris sit amet ante urna. Etiam laoreet nibh a purus condimentum, at pharetra ligula fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget venenatis quam, 
+                    non dapibus magna. Curabitur fringilla felis in dui fringilla efficitur. Aliquam erat volutpat. 
+                    Nam eget lorem eu orci rutrum lacinia. Donec ultricies augue non urna suscipit, 
+                    quis ornare libero suscipit. Phasellus facilisis sit amet justo eget ullamcorper. 
+                    Sed eu augue ac lacus lacinia sodales. Fusce varius libero eget felis egestas, a elementum nisi posuere.
+                    Suspendisse potenti. Quisque sit amet nibh ac arcu volutpat scelerisque. 
+                    Etiam id ultricies ex, non laoreet leo. Pellentesque a fringilla metus, sed maximus augue. 
+                    Vivamus congue vehicula dui, a fermentum libero condimentum ac. 
+                    Mauris sit amet ante urna. Etiam laoreet nibh a purus condimentum, at pharetra ligula fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget venenatis quam, 
+                    non dapibus magna. Curabitur fringilla felis in dui fringilla efficitur. Aliquam erat volutpat. 
+                    Nam eget lorem eu orci rutrum lacinia. Donec ultricies augue non urna suscipit, 
+                    quis ornare libero suscipit. Phasellus facilisis sit amet justo eget ullamcorper. 
+                    Sed eu augue ac lacus lacinia sodales. Fusce varius libero eget felis egestas, a elementum nisi posuere.
+                    Suspendisse potenti. Quisque sit amet nibh ac arcu volutpat scelerisque. 
+                    Etiam id ultricies ex, non laoreet leo. Pellentesque a fringilla metus, sed maximus augue. 
+                    Vivamus congue vehicula dui, a fermentum libero condimentum ac. 
+                    Mauris sit amet ante urna. Etiam laoreet nibh a purus condimentum, at pharetra ligula fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget venenatis quam, 
+                    non dapibus magna. Curabitur fringilla felis in dui fringilla efficitur. Aliquam erat volutpat. 
+                    Nam eget lorem eu orci rutrum lacinia. Donec ultricies augue non urna suscipit, 
+                    quis ornare libero suscipit. Phasellus facilisis sit amet justo eget ullamcorper. 
+                    Sed eu augue ac lacus lacinia sodales. Fusce varius libero eget felis egestas, a elementum nisi posuere.
+                    Suspendisse potenti. Quisque sit amet nibh ac arcu volutpat scelerisque. 
+                    Etiam id ultricies ex, non laoreet leo. Pellentesque a fringilla metus, sed maximus augue. 
+                    Vivamus congue vehicula dui, a fermentum libero condimentum ac. 
+                    Mauris sit amet ante urna. Etiam laoreet nibh a purus condimentum, at pharetra ligula fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget venenatis quam, 
+                    non dapibus magna. Curabitur fringilla felis in dui fringilla efficitur. Aliquam erat volutpat. 
+                    Nam eget lorem eu orci rutrum lacinia. Donec ultricies augue non urna suscipit, 
+                    quis ornare libero suscipit. Phasellus facilisis sit amet justo eget ullamcorper. 
+                    Sed eu augue ac lacus lacinia sodales. Fusce varius libero eget felis egestas, a elementum nisi posuere.
+                    Suspendisse potenti. Quisque sit amet nibh ac arcu volutpat scelerisque. 
+                    Etiam id ultricies ex, non laoreet leo. Pellentesque a fringilla metus, sed maximus augue. 
+                    Vivamus congue vehicula dui, a fermentum libero condimentum ac. 
+                    Mauris sit amet ante urna. Etiam laoreet nibh a purus condimentum, at pharetra ligula fermentum.`;
+
+// Pagination settings
+let loadedText = '';
+let index = 0;
+const wordsPerLoad = 1000; // Number of words per load
+
+// Function to load more text
+function loadMore() {
+    const words = novelText.split(' ');
+    const nextChunk = words.slice(index, index + wordsPerLoad).join(' ');
+
+    loadedText += nextChunk + ' ';
+    document.getElementById('novel-text').textContent = loadedText;
+
+    index += wordsPerLoad;
+
+    // Hide button if all text is loaded
+    if (index >= words.length) {
+        document.getElementById('load-more-btn').style.display = 'none';
+    }
+}
+
+// Comment Functionality
+function addComment(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const comment = document.getElementById('comment').value;
+    const commentList = document.getElementById('comment-list');
+
+    // Create new comment element
+    const newComment = document.createElement('li');
+    newComment.innerHTML = `<strong>${name}:</strong> <p>${comment}</p>`;
+    
+    commentList.appendChild(newComment);
+
+    // Clear form fields
+    document.getElementById('comment-form').reset();
+}
